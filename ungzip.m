89b08d122e13da516ea943d4ee4faf3790309cee
immunvet <immunvet@gmail.com>
2019-03-02 17:47:43 -0500

    some changes

--- ungzip.m
+++ ungzip.m
@@ -3,10 +3,11 @@ clear; close all; clc;
 %newGuy = gunzip('./train-images-idx3-ubyte.gz');
 fid = fopen('train-images-idx3-ubyte', 'r', 'b');
 
+%%
 header = fread(fid, 1, 'int32');
 count = fread(fid, 1, 'int32');
 
-
+%%
 h = fread(fid, 1, 'int32');
 w = fread(fid, 1, 'int32');
 
@@ -20,11 +21,16 @@ end
 
 fclose(fid);
 
+
+
 %%
 lbls = fopen('train-labels-idx1-ubyte', 'r', 'b');
-labels=zeros([60000,1]);
-count = fread(lbls, 1, 'int32');
+
+
+%%
+labels = cell(60000,1);
+count = fread(lbls, 1);
 for i=1:count
-    labels(i,:) = fread(lbls, w, 'int');
+    labels(i,:) = fread(lbls, w );
 end
 
